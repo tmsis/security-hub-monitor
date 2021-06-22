91027200a315a0b1ad82c8a150d64bf4917d9e0e
@robot-mac-fc <robot-mac-fc@users.noreply.github.com>
2021-06-22 15:49:16 +0000

    Publish release

--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.730647b48f844aaeb16add2c8ab0b6ba.js"
+  "/precache-manifest.b604710748cdd8998b32d5fd716924e8.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -34,6 +34,6 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
-
+  
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
